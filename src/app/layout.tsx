3928734bb80eb5d6c91ac62a039158aeb1696ab1
飛岡佑太朗 <tobiokayuutaakira@tobiokayuutaakiranoMacBook-Pro.local>
2026-04-28 01:30:45 +0900

    Revert Google Site Verification code to previous version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "東京米国株カレッジ｜米国株・株式投資を初心者から学ぶ",
   description: "5年で1300%以上の実績を持つ米国株長期投資コミュニティ。株式投資の基礎から、投資初心者でも迷わず資産を築ける考え方を共有します。",
   verification: {
-    google: "8jgw025GuuH08YZY5rnsDSCY0rGdGzLRPvnpKiu2uV4",
+    google: "uPPhCDKZb278y7qY5uJ76kC0Vd1YEEe972xDjb9bkx0",
   },
 };
 
