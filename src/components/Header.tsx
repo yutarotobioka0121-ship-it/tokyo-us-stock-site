aa3f8c08f8a081b042a8f11d869104947be09f3a
飛岡佑太朗 <tobiokayuutaakira@tobiokayuutaakiranoMacBook-Pro.local>
2026-05-08 08:58:08 +0900

    Remove '勉強会申し込み' from Header navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,6 @@ export default function Header() {
           <Link href="/about" className="nav-link">自己紹介</Link>
           <Link href="/seminar" className="nav-link">勉強会詳細</Link>
           <Link href="/blog" className="nav-link">ブログ</Link>
-          <Link href="/apply" className="nav-link">勉強会申し込み</Link>
           <Link href="/contact" className="nav-link">お問い合わせ</Link>
         </nav>
 
@@ -46,7 +45,6 @@ export default function Header() {
           <Link href="/about" className="nav-link" onClick={() => setIsOpen(false)}>自己紹介</Link>
           <Link href="/seminar" className="nav-link" onClick={() => setIsOpen(false)}>勉強会詳細</Link>
           <Link href="/blog" className="nav-link" onClick={() => setIsOpen(false)}>ブログ</Link>
-          <Link href="/apply" className="nav-link" onClick={() => setIsOpen(false)}>勉強会申し込み</Link>
           <Link href="/contact" className="nav-link" onClick={() => setIsOpen(false)}>お問い合わせ</Link>
         </nav>
       </div>
